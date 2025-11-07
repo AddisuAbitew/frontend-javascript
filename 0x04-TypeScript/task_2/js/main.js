04263eaca7cded8e316f6c4f8117887d0cd03bc5
AddisuAbitew <addisuabitew1@gmail.com>
2025-11-07 15:34:55 +0300

    Add config files

---
+++ 0x04-TypeScript/task_2/js/main.js
@@ -0,0 +1,49 @@
+var Director = /** @class */ (function () {
+    function Director() {
+    }
+    Director.prototype.workFromHome = function () {
+        return 'Working from home';
+    };
+    Director.prototype.getCoffeeBreak = function () {
+        return 'Getting a coffee break';
+    };
+    Director.prototype.workDirectorTasks = function () {
+        return 'Getting to director tasks';
+    };
+    return Director;
+}());
+var Teacher = /** @class */ (function () {
+    function Teacher() {
+    }
+    Teacher.prototype.workFromHome = function () {
+        return 'Cannot work from home';
+    };
+    Teacher.prototype.getCoffeeBreak = function () {
+        return 'Cannot have a break';
+    };
+    Teacher.prototype.workTeacherTasks = function () {
+        return 'Getting to work';
+    };
+    return Teacher;
+}());
+var createEmployee = function (salary) {
+    if (typeof (salary) == 'number' && salary < 500) {
+        return new Teacher();
+    }
+    return new Director();
+};
+function isDirector(employee) {
+    return employee instanceof Director;
+}
+function executeWork(employee) {
+    if (isDirector(employee)) {
+        return employee.workDirectorTasks();
+    }
+    else {
+        return employee.workTeacherTasks();
+    }
+}
+var emp1 = createEmployee(200); // Teacher
+var emp2 = createEmployee(1000); // Director
+console.log(executeWork(emp1)); // Output: Getting to work
+console.log(executeWork(emp2)); // Output: Getting to director tasks
